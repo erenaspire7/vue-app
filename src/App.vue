2270e1e094f32540ac5b3ff94083f181a9eb655e
Yusuff Jamal <yusuffjamal3@gmail.com>
2022-01-11 13:35:19 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import Form from './components/Form.vue'
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" class="mx-auto"/>
+  <Form />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1  class="mt-12 text-3xl font-semibold">Test Form</h1>
+    <form class="w-80 mx-auto space-y-8" v-on:submit="submitForm($event)">
+        <div class="flex flex-col" >
+            <label class="text-left">Name</label>
+            <input class="border-2 rounded-md p-2 border-gray-500" required name="name"/>
+        </div>
+
+        <div class="flex flex-col" >
+            <label class="text-left">Email</label>
+            <input class="border-2 rounded-md p-2 border-gray-500" required/>
+        </div>
+
+        <div class="flex flex-col">
+            <label class="text-left">Your Message</label>
+            <textarea class="border-2 rounded-md p-2 border-gray-500" required/>
+        </div>
+
+        <button class="px-6 py-2 bg-emerald-500 rounded-md text-white" type="submit">Submit</button>
+    </form> 
+</template>
+
+<script>
+    export default {
+        name: "Form",
+        methods: {
+            submitForm: function (event) {
+                event.preventDefault()
+
+                alert(`Name: ${event.target.name.value}`)
+            }
+        }
+    }
+</script>
